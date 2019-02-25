c696950d5416bf92c8f5a4088b9698287fe1c105
Richard <rmd127@miami.edu>
2019-02-25 03:30:59 -0500

    Login Screen + account creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -18,9 +19,19 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
+        textTheme: TextTheme(
+          body1: new TextStyle(color: Colors.red),
+          display1: new TextStyle(color: Colors.white),
+          display2: new TextStyle(color: Colors.white),
+          display3: new TextStyle(color: Colors.white),
+          display4: new TextStyle(color: Colors.white),
+          body2: new TextStyle(color: Colors.white),
+          headline: new TextStyle(color: Colors.white),
+          title: new TextStyle(color: Colors.white),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'SpotBuddy Login'),
     );
   }
 }
@@ -47,6 +58,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    var url = "http://13.59.182.232/createProfile";
+    //var url = "http://18.222.104.22/createProfile";
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
@@ -54,6 +67,22 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      http.post(url, body: {
+        "first_name": "App Post",
+        "email": "app@app.com",
+        "pass": "******",
+        "birthday": "2-5-1978",
+        "messangerID": "SomeID",
+        "broadLocationID": "SomeID",
+        "focusedLocationID": "Some ID",
+        "biography": "Posting From App",
+        "isABuddy": "1",
+        "publicRating": "4.6",
+        "govtID": "Good Question"
+      }).then((response) {
+        print("Response status: ${response.statusCode}");
+        print("Response body: ${response.body}");
+      });
     });
   }
 
@@ -69,12 +98,12 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title, style: TextStyle(color: Colors.white)),
       ),
-      body: Center(
+      body: SafeArea(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: ListView(
           // Column is also layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -89,14 +118,37 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            SizedBox(height: 80.0),
+            SizedBox(height: 80.0), // Space for Logo
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Username",
+                filled: true,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            SizedBox(height: 80.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Password",
+                filled: true,
+              ),
+              obscureText: true,
+            ),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text('CANCEL'),
+                  onPressed: () {},
+                ),
+                RaisedButton(
+                  child: Text("Next"),
+                  onPressed: () {
+                    _incrementCounter();
+                  },
+                )
+              ],
             ),
           ],
         ),
